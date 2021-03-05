a9aaa34897ba8a3939230127ddabcca770c99c36
Maksym Chernozhukov <50479537+Im-Watching-You@users.noreply.github.com>
2021-03-05 21:18:22 +0900

    Updated files

--- Interview/fibonacci_sequence.py
+++ Interview/fibonacci_sequence.py
@@ -1,20 +1,83 @@
 """Fibonacci Sequence"""
 
-# # Fibonacci
-# a, b = 0, 1
-# for _ in range(10):
-#     print(a)
-#     a, b = b, a + b
+from functools import lru_cache
 
 
-def fib(num):
-    """Fibonacci generator"""
-    a, b = 0, 1
-    for i in range(num):
-        yield '{}: {}'.format(i+1, a)
-        a, b = b, a + b
+class Fibonacci:
+    """Fibonacci Sequence"""
+
+    def __init__(self, num):
+        """param num: int, number of Fibonacci sequences"""
+        self.num = num
+
+    def fib_1(self):
+        """Version 1: Short Fibonacci"""
+        a, b = 1, 1
+        for i in range(self.num):
+            print(i + 1, ':', a)
+            a, b = b, a + b
+
+    def fib_2(self):
+        """Version 2: Fibonacci Generator"""
+
+        def fibonacci(n):
+            """Calculate the Fibonacci Sequence with yield"""
+            a, b = 1, 1
+            for i in range(n):
+                yield '{} : {}'.format(i + 1, a)
+                a, b = b, a + b
+
+        # Execution
+        for item in fibonacci(self.num):
+            print(item)
+
+    def fib_3(self):
+        """Version 3: Fibonacci with fibonacci_cache dictionary memorization"""
+        fibonacci_cache = {}
+
+        def fibonacci(n):
+            """Calculate the Fibonacci Sequence with cache dictionary"""
+            value = 0
+
+            if n in fibonacci_cache:
+                return fibonacci_cache[n]
+
+            if n == 1:
+                value = 1
+            elif n == 2:
+                value = 1
+            elif n > 2:
+                value = fibonacci(n - 1) + fibonacci(n - 2)
+
+            fibonacci_cache[n] = value
+            return value
+
+        # Execution
+        for i in range(self.num):
+            print(i + 1, ':', fibonacci(i + 1))
+
+    def fib_4(self):
+        """Version 4: Fibonacci with lru_cache memorization"""
+
+        @lru_cache(maxsize=1000)
+        def fibonacci(n):
+            """Calculate the Fibonacci Sequence with lru_cache"""
+
+            if n == 1:
+                return 1
+            elif n == 2:
+                return 1
+            elif n > 2:
+                return fibonacci(n - 1) + fibonacci(n - 2)
+
+        # Execution
+        for i in range(self.num):
+            print(i + 1, ':', fibonacci(i + 1))
 
 
-# Execution
-for item in fib(10):
-    print(item)
+number = 1000
+fibonacci = Fibonacci(number)
+fibonacci.fib_1()
+# fibonacci.fib_2()
+# fibonacci.fib_3()
+# fibonacci.fib_4()
--- Interview/message_decoding.py
+++ Interview/message_decoding.py
@@ -17,22 +17,22 @@ def nuw_ways(num):
     count = 0
     num = str(num)
 
-    if num == '':
+    if num == '':                       # If data not invalid
         return 'Empty'
     if (len(num) > 2) | (num[0] in ['0', '-', ' ']):
         return 'Out of Length'
-    elif len(num) == 1:
+    elif len(num) == 1:                 # For data from 1-9
         count += 1
-        print(storage[num])
-    elif len(num) == 2:
+        print(num, ':', storage[num])
+    elif len(num) == 2:                 # For data from 10-26
         for n in num:
             count += 1
-            print(storage[n])
+            print(n, ':', storage[n])
         count += 1
-        print(storage[num])
+        print(num, ':', storage[num])
 
     return count
 
 
-data = '12'     # data = [1 - 26] | [a - z]
-print(nuw_ways(data))
+data = '26'     # data = [1 - 26] | [a - z]
+print('Total ways:', nuw_ways(data))
